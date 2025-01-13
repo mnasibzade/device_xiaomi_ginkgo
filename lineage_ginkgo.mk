d067dde42a73620767190467608c464ae8e67787
mnasibzade <euoryexe@gmail.com>
2025-01-13 13:14:39 +0400

    ginkgo: Change product to Lineage - aosp_ginkgo -> lineage_ginkgo.mk
    Signed-off-by: mnasibzade <euoryexe@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -25,9 +25,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
-# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
