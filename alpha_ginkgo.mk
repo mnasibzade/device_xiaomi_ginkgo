bd00278cf5cbb586e8e58ba599eab129a1318374
mnasibzade <euoryexe@gmail.com>
2025-01-22 15:13:08 +0400

    ginkgo: AlphaDroid 3.1 bringup - Hello alpha-15.1!
    Signed-off-by: mnasibzade <euoryexe@gmail.com>

--- alpha_ginkgo.mk
+++ alpha_ginkgo.mk
@@ -16,7 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 # Inherit framework first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -25,11 +24,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
+
+# Inherit some common AlphaDroid stuff.
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
+
+# Device config
+TARGET_HAS_UDFPS := false
+TARGET_ENABLE_BLUR := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Alpha Config
+TARGET_INCLUDE_LAWNCHAIR := true
+TARGET_INCLUDE_FENNEC := true
+TARGET_INCLUDE_MATLOG := false
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Maintainer
+ALPHA_BUILD_TYPE := Official
+ALPHA_MAINTAINER := Munir
 
 # Device identifier
-PRODUCT_NAME := aosp_ginkgo
+PRODUCT_NAME := alpha_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
